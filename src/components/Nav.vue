4e663b548fdb1a35d113beb440f5f6563a267a3b
OXAYR <uzairmaqsood451@gmail.com>
2023-09-24 01:32:19 +0500

    did some fixes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="navBar">
-    <nav class="py-5 fixed top-0 left-0 right-0 font-serif">
+    <nav class="py-5 bg-white fixed top-0 left-0 right-0 font-serif">
       <div class="flex justify-between items-center px-10">
         <div class="flex col-span-7">
           <router-link to="/home">
@@ -14,19 +14,19 @@
         <div class="flex">
           <router-link
             to="/home/user/cars"
-            class="shadow-sm text-rose-500 hover:bg-rose-500 hover:text-black rounded p-2 ml-4 mt-1">
+            class="text-rose-500 hover:bg-rose-500 hover:text-black rounded p-2 ml-4 mt-1">
             Cars
           </router-link>
           <router-link
             to="/home/user/reservations"
-            class="shadow-sm text-rose-500 hover:bg-rose-500 hover:text-black rounded p-2 ml-4 mt-1 mr-4">
+            class="text-rose-500 hover:bg-rose-500 hover:text-black rounded p-2 ml-4 mt-1 mr-4">
             Reservations
           </router-link>
         </div>
         <div class="flex">
           <router-link
             to="/home/user/account"
-            class="shadow-sm text-black bg-rose-500 hover:bg-rose-600 rounded p-2 ml-4 mt-1">
+            class="text-black bg-rose-500 hover:bg-rose-600 rounded p-2 ml-4 mt-1">
             {{ name }}
           </router-link>
         </div>
--- src/components/user/Banner.vue
+++ src/components/user/Banner.vue
@@ -4,11 +4,18 @@
       <h1 class="text-5xl">
         Driving <span class="text-rose-500">Dreams</span>, One Rental at a Time
       </h1>
-      <p>
+      <p class="mt-4">
         Welcome to our premier online destination for hassle-free car rentals.
         Discover a wide range of vehicles, competitive rates, and seamless
         booking experiences. Start your journey with us today!
       </p>
+      <div class="mt-8">
+        <router-link
+          class="bg-rose-500 w-32 text-center py-3 px-3 rounded-lg text-white shadow-sm"
+          to="/home/user/cars">
+          Book me a car
+        </router-link>
+      </div>
     </div>
     <div class="mt-20">
       <img src="../../assets/car3.png" alt="" />
